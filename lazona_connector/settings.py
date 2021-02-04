24821676a4b8b173c1aa72983655f5d4919dd758
Pau Perez <saulopefa@gmail.com>
2021-02-04 16:19:28 +0100

    Fetch secret key from Heroku's config env var

--- lazona_connector/settings.py
+++ lazona_connector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'da2l%!4lyikq!)uct0tdyv$tv*wae(6)rten)koq(tt-2*5nsb'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
