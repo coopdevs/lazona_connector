4fb391ee1a4897a024af258b8588aa06d95e0ffb
Pau Perez <saulopefa@gmail.com>
2021-02-04 16:20:44 +0100

    Disable static file serving
    So far we have no plans on using this as this will be an API-only
    project.

--- lazona_connector/settings.py
+++ lazona_connector/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -117,4 +116,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+#  STATIC_URL = '/static/'
