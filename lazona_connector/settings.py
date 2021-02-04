0ee6d324db35a0e8c4e5f813680f88b49a39ffed
Pau Perez <saulopefa@gmail.com>
2021-02-04 16:19:43 +0100

    Allow to server requests from Heroku

--- lazona_connector/settings.py
+++ lazona_connector/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["lazona-connector-staging.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
