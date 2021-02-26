1f8f823a014daed8575bf0e1175d8acb8abdfcda
Pau Perez <saulopefa@gmail.com>
2021-02-26 13:41:41 +0100

    Create Koiki delivery from DeliveryList view

--- api/tests.py
+++ api/tests.py
@@ -3,8 +3,15 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from unittest.mock import patch
+from unittest.mock import MagicMock
 
 from .models import Delivery
+from api.views import Client
+
+# TODO: Move me to tests/__init__.py and make sure is still protecting us
+import httpretty
+httpretty.enable(allow_net_connect=False)
 
 class DeliveryModelTests(APITestCase):
 
@@ -29,3 +36,45 @@ class DeliveryViewTests(TestCase):
 
         self.assertEqual(response.data, {'order_number': ['This field is required.']})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    @patch('api.views.Client', autospec=True)
+    def test_koiki_client_is_used(self, mock_koiki_client):
+        url = reverse('deliveries:create')
+        data = {'order_number': 'xxx'}
+
+        httpretty.register_uri(httpretty.POST, 'https://testing_host/rekis/api/altaEnvios',
+                               status=200, content_type='text/json')
+
+        response = self.client.post(url, data, format='json')
+
+        mock_koiki_client.assert_called_once_with({
+            'order_key': 'xxx',
+            'shipping': {
+                'first_name': 'John',
+                'last_name': 'Lennon',
+                'address_1': 'Beatles Street 66',
+                'address_2': '',
+                'postcode': '08032',
+                'city': 'Barcelona',
+                'state': 'Barcelona',
+                'country': 'Spain'
+            },
+            'billing': {
+                'phone': '666666666',
+                'email': 'lennon@example.com'
+            }
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    @patch('api.views.Client.create_delivery', autospec=True)
+    def test_koiki_create_delivery_is_called(self, mock_create_delivery):
+        url = reverse('deliveries:create')
+        data = {'order_number': 'xxx'}
+
+        httpretty.register_uri(httpretty.POST, 'https://testing_host/rekis/api/altaEnvios',
+                               status=200, content_type='text/json')
+
+        response = self.client.post(url, data, format='json')
+
+        mock_create_delivery.assert_called()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- api/views.py
+++ api/views.py
@@ -2,10 +2,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from api.serializers import DeliverySerializer
+from koiki.client import Client
 
 class DeliveryList(APIView):
     def post(self, request):
         serializer = DeliverySerializer(data=request.data)
         if serializer.is_valid():
+            order = {
+                'order_key': 'xxx',
+                'shipping': {
+                    'first_name': 'John',
+                    'last_name': 'Lennon',
+                    'address_1': 'Beatles Street 66',
+                    'address_2': '',
+                    'postcode': '08032',
+                    'city': 'Barcelona',
+                    'state': 'Barcelona',
+                    'country': 'Spain'
+                },
+                'billing': {
+                    'phone': '666666666',
+                    'email': 'lennon@example.com'
+                }
+            }
+            Client(order).create_delivery()
+
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
