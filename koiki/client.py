15c3b94f775b9dde2e59ad226d23ad8c9fc69804
Pau Perez <saulopefa@gmail.com>
2021-03-22 13:16:47 +0100

    DRY calls to logger in Client

--- koiki/client.py
+++ koiki/client.py
@@ -21,19 +21,18 @@ class Client():
 
     def create_delivery(self):
         endpoint_req = CreateDelivery(self.order)
-        body = self._authentication(endpoint_req)
+        req_body = self._authentication(endpoint_req)
 
-        self.logger.info('Koiki request. body=%s', body)
+        self.logger.info('Koiki request. body={}'.format(req_body))
 
-        response = requests.post(self._url(endpoint_req), json=body)
+        response = requests.post(self._url(endpoint_req), json=req_body)
         response_body = json.loads(response.text)
 
         if self._is_errored(response_body):
-            self._log_error(response.status_code, response.text)
+            self._log(response.status_code, response.text, logger='error')
             return Error(response_body)
         else:
-            self.logger.info(
-                'Koiki response. status=%s, body=%', response.status_code, response.text)
+            self._log(response.status_code, self._masked_body(response_body))
             return Delivery(response_body['envios'][0])
 
     def _url(self, endpoint_req):
@@ -53,5 +52,9 @@ class Client():
     def _is_errored(self, response_body):
         return response_body.get('respuesta', '') != '101'
 
-    def _log_error(self, code, msg):
-        self.logger.error('Failed request. status=%s, body=%s', code, msg)
+    def _log(self, code, msg, logger='info'):
+        log_line = "Koiki response. status={}, body={}".format(code, msg)
+        if logger == 'info':
+            self.logger.info(log_line)
+        else:
+            self.logger.error(log_line)
