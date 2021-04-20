30bd5e3e62d5abbe039424990cf650cba6fadd38
Pau Perez <saulopefa@gmail.com>
2021-04-20 16:57:34 +0200

    Use 9-digit phone number in Sender
    Closes #37. We mustn't forget to use the vendor's contact details
    when we change this.

--- koiki/sender.py
+++ koiki/sender.py
@@ -2,6 +2,7 @@ class Sender():
 
     """Encapsulates the sender information to be provided to all Koiki deliveries"""
 
+    # Note telefonoRemi can only have 9 digits.
     def to_dict(self):
         return {
             'nombreRemi': 'La Zona',
@@ -13,5 +14,5 @@ class Sender():
             'provinciaRemi': 'Barcelona',
             'paisRemi': 'ES',
             'emailRemi': 'lazona@opcions.org',
-            'telefonoRemi': '+34518888191',
+            'telefonoRemi': '518888191',
         }
