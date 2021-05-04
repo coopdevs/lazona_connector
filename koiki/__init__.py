aa6fbda43d86d8bd7ea6410ce79f34924a1de8f5
Pau Perez <saulopefa@gmail.com>
2021-05-04 15:22:15 +0200

    Match ENV var name to Python var name

--- koiki/__init__.py
+++ koiki/__init__.py
@@ -2,6 +2,6 @@ import os
 import logging
 
 host = os.getenv('KOIKI_HOST')
-wcfmmp_host = os.getenv('WCFMMP_API_BASE')
+wcfmmp_host = os.getenv('WCFMMP_HOST')
 logger = logging.getLogger('django.server')
 auth_token = os.getenv('KOIKI_AUTH_TOKEN')
