ea59245d5f9d7a7284d4a9923f71d26a6e87bc78
Pau Perez <saulopefa@gmail.com>
2021-05-04 08:26:15 +0200

    Fix missing wcfmmp_host config setting

--- koiki/__init__.py
+++ koiki/__init__.py
@@ -2,5 +2,5 @@ import os
 import logging
 
 host = os.getenv('KOIKI_HOST')
-wcfmmp_api_base = os.getenv('WCFMMP_API_BASE')
+wcfmmp_host = os.getenv('WCFMMP_API_BASE')
 logger = logging.getLogger('django.server')
