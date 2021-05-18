e87e6ff9d75b22ec9b885868c7a478aed0ea51f3
Kapis <kapis@riseup.net>
2021-05-18 23:08:47 +0200

    Added Get field test

---
+++ sugarcrm/tests/test_get_field.py
@@ -0,0 +1,37 @@
+from unittest import TestCase
+from unittest.mock import patch, MagicMock, call
+import json
+from sugarcrm.client import APIClient
+from sugarcrm.error import CrmResponseError
+
+
+class SearchTest(TestCase):
+    def setUp(self):
+        self.mock_client = MagicMock()
+        self.object_id = "CONTACT_RECORD_ID"
+        self.field_to_get = "myfield"
+        self.value_to_get = "FIELD_VALUE_ID"
+
+    def test_success_get_field_request(self):
+        http_mock_response = MagicMock()
+        self.mock_client.get = http_mock_response
+        api_client = APIClient(self.mock_client)
+        http_mock_response.return_value.json.return_value = {
+                "entry_list": [
+                    {
+                        "name_value_list": {self.field_to_get: {"value": self.value_to_get}},
+                    }
+                ]
+        }
+        value = api_client.get_field("Module", self.object_id, self.field_to_get)
+
+        self.assertEqual(self.value_to_get, value)
+
+    def test_wrong_get_field_request(self):
+        http_mock_response = MagicMock()
+        self.mock_client.get = http_mock_response
+        api_client = APIClient(self.mock_client)
+
+        http_mock_response.return_value.json.return_value = {}
+        with self.assertRaises(CrmResponseError):
+            _ = api_client.get_field("Module", self.object_id, self.field_to_get)
