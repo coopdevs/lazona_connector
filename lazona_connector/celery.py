0078e43db222c2b2af84caa86f2c4d6e220f475f
Pau Perez <saulopefa@gmail.com>
2021-05-18 13:43:58 +0200

    Explicitly configure Celery's backend and broker
    Configuring Celery's results backend is becoming a bit tricky.
    Apparently, as I've seen in https://blog.heroku.com/securing-celery,
    Redis doesn't support SSL so we need to purposefully disable SSL while
    using Redi's secure protocol (rediss://).
    
    That's why I decided to make Celery's configuration a bit more explicit
    and grepeable moving it from ENV vars only to code. Without
    `ssl_cert_reqs` things won't work on Heroku so better to have a bit more
    obvious.

--- lazona_connector/celery.py
+++ lazona_connector/celery.py
@@ -5,7 +5,11 @@ import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lazona_connector.settings')
 
-app = Celery('lazona_connector')
+app = Celery(
+    'lazona_connector',
+    broker=os.environ['REDIS_URL'],
+    backend=f'{os.environ["REDIS_URL"]}?ssl_cert_reqs=none'
+)
 app.config_from_object(settings)
 app.autodiscover_tasks()
 
