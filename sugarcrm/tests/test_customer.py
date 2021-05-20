ea3689a78199a145459cf2ee5e8fd2c1f0574007
Kapis <kapis@riseup.net>
2021-05-20 14:09:37 +0200

    added coverage test for tasks.py

--- sugarcrm/tests/test_customer.py
+++ sugarcrm/tests/test_customer.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 from sugarcrm.customer import Customer
+from api.tasks import _check_customer_is_partner
 
 
 class CustomerTest(TestCase):
@@ -28,3 +29,10 @@ class CustomerTest(TestCase):
         customer.fetch(self.email)
         self.assertNotIn("^is_partner^", customer.roles)
         self.assertFalse(customer.check_is_partner())
+
+    @patch("sugarcrm.customer.APIClient", autospec=True)
+    def test_check_customer_is_not_partner(self, mock_client):
+        mock_client.return_value = self.mock_client
+        self.mock_client.search_email.return_value = ("ACCOUNT_ID", "CONTACT_ID")
+        self.mock_client.get_field.return_value = self.roles_from_crm_not_as_partner
+        self.assertFalse(_check_customer_is_partner(self.email))
