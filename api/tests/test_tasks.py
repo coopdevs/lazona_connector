f75ac6df51e80216c7e717baffb5fb749317ea49
Kapis <kapis@riseup.net>
2021-06-17 23:44:19 +0200

    Fixed test and added email test

--- api/tests/test_tasks.py
+++ api/tests/test_tasks.py
@@ -1,15 +1,22 @@
-from django.test import TestCase
 from unittest.mock import patch, MagicMock
+import responses
+from django.template.loader import render_to_string
+from django.utils.translation import gettext as _
+from django.test import TestCase
 
 from api.serializers import OrderSerializer
 from api.tasks import create_delivery
-from koiki.error import Error
+import koiki
+from koiki.delivery import Delivery
+
+
 
 
 class TasksTests(TestCase):
 
     def setUp(self):
         self.data = {
+            'id': 33,
             'order_key': 'xxx',
             'customer_note': '',
             'shipping': {
@@ -40,11 +47,27 @@ class TasksTests(TestCase):
             }]
         }
 
+        responses.add(responses.GET, f'{koiki.wcfmmp_host}/wp-json/wcfmmp/v1/settings/id/6',
+                      status=200,
+                      json={
+                        "store_email": "test@test.es",
+                        "phone": "",
+                        "address": {
+                            "street_1": "",
+                            "street_2": "",
+                            "city": "",
+                            "zip": "",
+                            "country": "ES",
+                            "state": ""
+                        }
+                      })
+
     @patch('api.tasks.Client', autospec=True)
-    def test_pdf_persistance(self, mock_client):
+    def test_delivery_successful(self, mock_client):
         mock_delivery = MagicMock(name='delivery')
         client = MagicMock(name='client')
-        client.create_delivery.return_value = mock_delivery
+        client.create_delivery.return_value = [mock_delivery]
+        mock_delivery._is_errored.return_value = False
         mock_delivery.print_pdf.return_value = MagicMock()
 
         mock_client.return_value = client
@@ -54,30 +77,55 @@ class TasksTests(TestCase):
         order = serializer.validated_data
         create_delivery(order)
 
-        mock_delivery.print_pdf.assert_called_once()
+        mock_delivery.send_mail_to_vendor.assert_called_once()
 
     @patch('api.tasks.Client', autospec=True)
     def test_failure(self, mock_client):
         client = MagicMock()
-        client.create_delivery.return_value = Error({'mensaje': 'ERROR IN THE RECEIVED DATA'})
+        vendor = MagicMock()
+        delivery = Delivery({
+            "respuesta": "102",
+            "mensaje": "ERROR IN THE RECEIVED DATA", "numPedido": "test", "order_id": 33},
+            vendor)
+        client.create_delivery.return_value = [
+            delivery
+        ]
         mock_client.return_value = client
 
         serializer = OrderSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
         order = serializer.validated_data
-        result = create_delivery(order)
+        create_delivery(order)
+        self.assertTrue(delivery._is_errored())
 
-        self.assertEqual(result, {'error': 'ERROR IN THE RECEIVED DATA'})
-
-    @patch('api.tasks.Client', autospec=True)
-    def test_success(self, mock_client):
-        client = MagicMock()
-        client.create_delivery.return_value = MagicMock()
-        mock_client.return_value = client
+    @responses.activate
+    @patch('koiki.delivery.EmailMessage', autospec=True)
+    @patch('koiki.delivery.logger', autospec=True)
+    def test_create_delivery_sends_email(self, mock_logger, mock_email):
+        responses.add(responses.POST, 'https://testing_host/rekis/api/altaEnvios', status=200,
+                      json={
+                        'respuesta': '101',
+                        'mensaje': 'OK',
+                        'envios': [{'numPedido': '123', 'codBarras': 'yyy', 'etiqueta': 'abcd',
+                                    'respuesta': '101', 'mensaje': 'OK'}]
+                      })
 
         serializer = OrderSerializer(data=self.data)
         self.assertTrue(serializer.is_valid())
         order = serializer.validated_data
-        result = create_delivery(order)
 
-        self.assertEqual(result, {'success': 'all good'})
+        mock_email.send.return_value = True
+        context = {
+            'wcfmmp_host': koiki.wcfmmp_host,
+            'wc_order_id': 33,
+        }
+
+        message = render_to_string('contact_template.txt', context)
+        create_delivery(order)
+        mock_logger.info.assert_called_once_with(
+            "Sending Koiki pdf to email test@test.es")
+        mock_email.assert_called_once_with(
+            _("Enviament Koiki per a la comanda: {}").format(33),
+            message,
+            to=["test@test.es"]
+        )
