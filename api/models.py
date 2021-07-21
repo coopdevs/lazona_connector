2a78c8e1b2b90c3c75756922d23fcb79cf2fc321
Kapis <kapis@riseup.net>
2021-07-21 21:36:12 +0200

    translated admin labels.

--- api/models.py
+++ api/models.py
@@ -8,12 +8,12 @@ class ShipmentStatus(models.TextChoices):
 
 
 class Shipment(models.Model):
-    delivery_id = models.CharField(max_length=100, null=False, blank=True, unique=False)
-    order_id = models.IntegerField(null=False, blank=False)
-    vendor_id = models.IntegerField(null=False, blank=False)
-    label_url = models.CharField(max_length=200, blank=True)
+    delivery_id = models.CharField(_("Id de l'enviament"), max_length=100, null=False, blank=True)
+    order_id = models.IntegerField(_("Id de la comanda de WooCommerce"), null=False, blank=False)
+    vendor_id = models.IntegerField(_("Id del vendor de WooCommerce"), null=False, blank=False)
+    label_url = models.CharField(_("Url de l'etiqueta"), max_length=200, blank=True)
     status = models.CharField(
-        _(u"Estat de l'enviament"),
+        _("Estat de l'enviament"),
         max_length=20,
         choices=ShipmentStatus.choices,
         null=False,
