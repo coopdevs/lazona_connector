e86c0d3ae360d579c72d176d2901f4e3663a8b53
Kapis <kapis@riseup.net>
2021-07-30 11:47:44 +0200

    fix lynt warning

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from unittest.mock import patch
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
