37113bb427c884ad69b474a2cf7f97531ae2a638
Kapis <kapis@riseup.net>
2021-08-06 18:33:01 +0200

    fixing the test

--- api/tests/test_vars.py
+++ api/tests/test_vars.py
@@ -12,5 +12,8 @@ class EnvVarsTests(TestCase):
     def test_create_shipment_successful(self, mock_vars):
         mock_vars.return_value = []
         importlib.reload(lazona_connector.vars)
-        self.assertEqual(lazona_connector.vars.koiki_host, os.getenv('SUGARCRM_PASSWORD'))
-        self.assertNotEqual(lazona_connector.vars.koiki_host, "test_sugarcrm_password")
+        self.assertEqual(lazona_connector.vars.sugarcrm_password, os.getenv('SUGARCRM_PASSWORD'))
+        self.assertNotEqual(lazona_connector.vars.sugarcrm_password, "test_sugarcrm_password")
+
+    def tearDown(self):
+        importlib.reload(lazona_connector.vars)
