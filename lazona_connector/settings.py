650c13707b5b63a429c17eb15d55c508ce212ced
Pere Picornell <pere@perepicornell.com>
2023-02-15 13:02:45 +0100

    settings: debug env var capturing modified

--- lazona_connector/settings.py
+++ lazona_connector/settings.py
@@ -52,7 +52,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = os.environ.get('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
